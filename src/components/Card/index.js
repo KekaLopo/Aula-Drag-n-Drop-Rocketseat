d07ff1de9028eae90c6b5a49d752372984ee0749
Joao Pedro Lopo <kekalopo@gmail.com>
2019-08-22 10:47:13 -0300

    Base do Site

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Label } from './styles';
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159C1" />
+      </header>    
+      <p>Fazer Concretagem do Pilar 1</p>
+      <img src="https://i.imgur.com/naOtrZX.jpg" />
+    </Container>
+  );
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './styles';
+
+export default function Header() {
+    return (
+        <Container>
+        <h1> Minha Obra</h1>            
+        </Container>
+    )
+}
